2e5d1a2787199f84f2b03274cb4c19dd28a46669
Rafaël Daigneau <rafael.daigneau@gmail.com>
2022-05-23 03:23:19 +0200

    Les paramètres du BrowserWindow (fenêtre)

--- main.js
+++ main.js
@@ -6,19 +6,26 @@ function createWindow() {
   const win = new BrowserWindow({
     width: 1280,
     height: 720,
+    minWidth: 1024,
+    minHeight: 640,
+    frame: true,
+    icon: path.join(__dirname, './ico.ico'),
     webPreferences: {
         preload: path.join(__dirname, 'preload.js'),
-        nodeIntegration: true
+        nodeIntegration: false,
+        contextIsolation: false,
+        devTools: true
     }
   });
     
     // and load the index.html of the app.
     win.loadFile(path.join(__dirname, 'index.html'));
+    win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
     createWindow();
-    
+
     app.on( 'activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
